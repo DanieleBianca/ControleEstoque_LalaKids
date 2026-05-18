e1cc5b2f129fc2fc5ff169d64ed86d8961fe406e
DanieleBianca <bianca.daniele1710@gmail.com>
2026-05-18 18:19:54 -0300

    Create Usuarios.cs

---
+++ Models/Usuarios.cs
@@ -0,0 +1,7 @@
+public class Usuario ////classe que representa um usuário, com propriedades e dados
+{
+    public string Id { get; set; }
+    public string Nome { get; set; }
+    public string Login { get; set; }
+    public string Senha { get; set; }
+}
